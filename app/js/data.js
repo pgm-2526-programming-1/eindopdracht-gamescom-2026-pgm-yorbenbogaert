e9ef9868df053748baa23b10f4340aa7d31e25e0
pgm-yorbenbogaert <yorben.bogaert@student.arteveldehs.be>
2025-11-07 13:13:33 +0100

    first commit

---
+++ app/js/data.js
@@ -0,0 +1,129 @@
+const navigationLink = [
+    { name: "Events",
+      link: "/app/index.html",
+      type: "internal"
+    },
+    { name: "Official Website",
+      link: "https://www.gamescom.global/en",
+      type: "external"
+    },
+    { name: "Graduaat Programmeren",
+      link: "https://www.arteveldehogeschool.be/opleidingen/graduaat/programmeren",
+      type: "external"
+    }
+];
+
+const eventcard = [
+    { id: "a2066ce61640402fb01f4f18a3a68602",
+      stage: "Innovation Stage",
+      event: {
+        name: "Next-Gen Game Engines",
+        description: "Join Tim Sweeney, CEO of Epic Games, as he explores the cutting-edge advancements in game engine technology. This session will cover the latest innovations in Unity’s development tools and their potential impact on the gaming industry. Attendees will gain insights into how these new technologies can enhance game development, improve performance, and enable more immersive and interactive experiences for players. Sweeney will also discuss upcoming features and tools designed to empower developers in creating next-generation games.",
+        socials: {
+            instagram: "https://www.instagram.com/epicgames/",
+            twitter: "https://x.com/EpicGames",
+            youtube: "https://www.youtube.com/@EpicGamesStore",
+            website: "https://www.epicgames.com/site/en-US/home",
+                 }
+            },
+        from: "1787734800000",
+        to: "1787742000000"
+    },
+    {
+        id: "439cc5151e654bfcb7501b74e10739a1",
+        stage: "Main Showcase Stage",
+        event: {
+            name: "Final Fantasy XVI",
+            description: "Step into the world of Final Fantasy XVI as Square Enix reveals the next wave of content for this epic fantasy RPG. In this presentation, fans will get an exclusive look at upcoming DLCs, gameplay expansions, and storylines that continue the journey of Clive Rosfield and the land of Valisthea. With stunning visuals, rich lore, and an immersive combat system, Final Fantasy XVI has redefined the series. The team at Square Enix will delve into new features, challenges, and fan-requested updates, offering a glimpse into how they are shaping the future of this beloved game. Don't miss this chance to witness the evolution of a modern classic.",
+            socials: {
+                instagram: "https://www.instagram.com/ffxiv/",
+                twitter: "https://x.com/FF_XIV_EN",
+                youtube: "https://www.youtube.com/@ffxiv",
+                website: "https://eu.finalfantasyxiv.com/"
+                    }
+            },
+        from: "1787738400000",
+        to: "1787743800000"
+ },
+    {
+        id: "8a4d0bbbf1d74f4db0b2ccd984a975d4",
+        stage: "Visionary Stage",
+        event: {
+            name: "Future of Open-World",
+            description: "Join renowned game director Hidetaka Miyazaki, the creative force behind Elden Ring and Dark Souls, for a deep dive into the evolving world of open-world game design. As technology advances and player expectations shift, the boundaries of what open-world games can achieve are being pushed further than ever. In this session, Miyazaki will explore emerging trends, innovations in game mechanics, and the future of player-driven exploration. He will also discuss how FromSoftware has redefined open-world storytelling, blending intricate world-building with player freedom. Discover how future titles will enhance immersion, AI-driven interactivity, and narrative depth, setting the stage for the next era of gaming.",
+            socials: {
+                instagram: "https://www.instagram.com/hidetaka.miyazaki_/",
+                twitter: "https://x.com/MiyazakiTweets",
+                website: "https://en.wikipedia.org/wiki/Hidetaka_Miyazaki",
+                     }
+            },
+        from: "1787749200000",
+        to: "1787756400000"
+    },
+    {
+        id: "987293a9f39c405da74727a91f1cadd5",
+        stage: "Main Showcase Stage",
+        event: {
+            name: "Starfield",
+            description: "Dive into the vast universe of Starfield with Bethesda Game Studios as they unveil the next phase of their groundbreaking space RPG. In this special presentation, fans will get an inside look at new content, expansions, and gameplay mechanics that take exploration to uncharted territories. Discover how Bethesda is pushing the boundaries of open-world design, blending cutting-edge technology with the freedom to explore the stars. The developers will also share behind-the-scenes insights into upcoming storylines, new planets, and enhancements to ship-building, crafting, and combat systems. Whether you're a seasoned space traveler or new to the journey, this event promises to show why Starfield is shaping the future of RPGs in the cosmos.",
+            socials: {
+                instagram: "https://www.instagram.com/starfieldgame/",
+                twitter: "https://x.com/StarfieldGame",
+                youtube: "https://www.youtube.com/@bethesda",
+                website: "https://bethesda.net/en/game/starfield",
+                     }
+            },
+        from: "1787745600000",
+        to: "1787752800000"
+
+    },
+    {
+        id: "a32947a31b3447ab89059d3ee2c401eb",
+        stage: "Innovation stage",
+        event: {
+            name: "AI and Machine Learning in Gaming",
+            description: "Explore the transformative impact of AI and machine learning on the gaming industry with Epic Games' CEO Tim Sweeney. In this insightful session, Sweeney will discuss how cutting-edge AI technology is revolutionizing game development, from creating more responsive and adaptive NPCs to procedural content generation and enhanced player experiences. Learn how machine learning is being harnessed to build smarter, more immersive worlds that evolve based on player choices and behavior. Sweeney will also provide a look at future possibilities, where AI-driven innovations could redefine the boundaries of creativity and interaction in gaming. Whether you're a developer or a gamer, this talk will offer a glimpse into the future of AI-powered gameplay.",
+            socials: {
+                instagram: "https://www.instagram.com/epicgames/",
+                youtube: "https://www.youtube.com/@EpicGamesStore",
+                website: "https://www.epicgames.com/site/en-US/home",
+                     }
+            },
+        from: "1787817600000",
+        to: "1787824800000"
+
+
+    },
+    {
+        id: "ac7b065f59cc4a688975a4bab84941d7",
+        stage: "Main Showcase Stage",
+        event: {
+            name: "Cyberpunk 2077: Phantom Liberty",
+            description: "CD Projekt Red returns to the dystopian streets of Night City with Phantom Liberty, the latest expansion for Cyberpunk 2077. In this exclusive showcase, the developers will unveil new storylines, gameplay mechanics, and major updates designed to elevate the Cyberpunk experience to new heights. Featuring fresh insights into the highly anticipated expansion, fans will get a closer look at the evolving world, including new characters, quests, and locations that expand the game's narrative universe. The team will also share key improvements based on player feedback, enhancing combat, AI, and world interaction. If you're a Cyberpunk fan or curious about the future of this genre-defining game, this session is a must-see.",
+            socials: {
+                instagram: "https://www.instagram.com/cyberpunkgame/",
+                twitter: "https://x.com/CyberpunkGame",
+                youtube: "https://www.cyberpunk.net/us/en/",
+                website: "https://www.youtube.com/@CyberpunkGame",
+                     }
+            },
+        from: "1787833800000",
+        to: "1787839200000"
+    },
+    {
+        id: "6b23d084653640699c356273366fe0bb",
+        stage: "Visionary Stage",
+        event: {
+            name: "Storytelling in the Digital Age",
+            description: "Legendary game writer and director Amy Hennig, known for her work on Uncharted and Legacy of Kain, delves into the evolving art of storytelling in modern video games. As the digital landscape expands, new technologies are transforming how narratives are built and experienced. In this insightful session, Hennig will explore the challenges and opportunities of crafting emotionally resonant, interactive stories that respond to player choices. She will share her experience creating character-driven narratives that go beyond traditional media, offering a glimpse into the future of storytelling where players are both audience and co-authors. Whether you're a developer, writer, or gamer, this talk provides a masterclass on shaping the future of interactive narrative design.",
+            socials: {
+                instagram: "https://www.instagram.com/amy_hennig/",
+                twitter: "https://x.com/amy_hennig",
+                website: "https://en.wikipedia.org/wiki/Amy_Hennig"
+
+
+                }
+    }
+}
+]
+
